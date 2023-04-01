71a3b6a52a3477423a3a25d4e249c714e0ad4407
R-Kyle <93040350+R-Kyle@users.noreply.github.com>
2023-04-02 00:39:53 +0800

    Add files via upload

---
+++ include/database.php
@@ -0,0 +1,7 @@
+<?php 
+
+// connecting to database
+$con = mysqli_connect("localhost", "root", "id20480444_textpertucc", "textpert") or die("Database Error");
+
+
+ ?>
---
+++ include/footer.php
@@ -0,0 +1,14 @@
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2023 Texpert</p>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact Us</a></li>
+      <li class="nav-item"><a href="C++.php" class="nav-link px-2 text-muted">C++</a></li>
+      <li class="nav-item"><a href="Java.php" class="nav-link px-2 text-muted">Java</a></li>
+      <li class="nav-item"><a href="PHP.php" class="nav-link px-2 text-muted">PHP</a></li>
+    </ul>
+  </footer>
+</div>
